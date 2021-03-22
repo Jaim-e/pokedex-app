f54f4dc38405cc196188bdca77246b43c8976c10
Jaim-e <51797216+Jaim-e@users.noreply.github.com>
2021-03-22 17:02:39 +0100

    Update scripts.js
    Displays the name of each Pokemon and its height, highlighting the bigger ones.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 let pokemonList = [
-  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-  { name: 'Charmander', height: 0.6, types: ['fire'] },
-  { name: 'Squirtle', height: 0.5, types: ['water'] }
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
+  { name: "Squirtle", height: 0.5, types: ["water"] }
 ];
+
+let text = " ";
+for (let i = 0; pokemonList[i]; i++) {
+  text = " " + pokemonList[i].name + " (Height: " + pokemonList[i].height + "m)";
+
+  if (pokemonList[i].height >= 0.7) {
+    document.write(text + " - Wow, that's big!" + "<br>");
+  } else {
+    document.write(text + "<br>");
+    }
+}
