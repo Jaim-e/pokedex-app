61bc37eed27884c7fc59b2308ae723955215e939
Jaim-e <51797216+Jaim-e@users.noreply.github.com>
2021-04-19 01:01:21 +0200

    Task 1.6
    Creating and addListItem( ) in my IIFE. Creating an event listener to each button for each Pokemon in the list.

--- js/scripts.js
+++ js/scripts.js
@@ -23,31 +23,47 @@ let pokemonRepository = (function () {
     if (pokemonList.find(item =>item.name === pokemon)) {
       console.log(pokemonList.filter(item => item.name.includes(pokemon)));
     } else {
-      console.log(pokemon + ' does not exists');
+      console.log(pokemon + " does not exists");
     }
   }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("custom-button");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    //Creating a function to add the event listener to the button:
+    addEvLiToButton(button, pokemon);
+  }
+  function addEvLiToButton(button, pokemon) {
+    //Adding an event listener to the variable "button":
+    button.addEventListener("click", function(event){
+    showDetails(pokemon);
+    })
+  }
+  //Creating a function that display the data from a pokemon when its button is clicked:
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
   return {
     add: add,
     getAll: getAll,
-    search: search
+    search: search,
+    addListItem: addListItem
   };
 })();
 
 pokemonRepository.add({ name: "Pikachu", height: 0.4, type: ["electric"] });
 
-pokemonRepository.getAll().forEach(function(item) {
-  text = item.name + " is a Pokémon of '" + item.type + "' type and is " + item.height + " m tall";
-  if (item.height > 1.5) {
-    document.write("<p>" + text + " - Wow, that's big!" + "<p/>");
-  } else if (item.height <= 0.4) {
-    document.write("<p>" + text + " - Well... at least it's cute!" + "<p/>");
-  } else {
-    document.write("<p>" + text + "<p/>");
-    }
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
 
 Object.keys(pokemonRepository.getAll()).forEach(function(property) {
-  console.log(property + ': ' + typeof(pokemonRepository.getAll()[property]));
+  console.log(property + ": " + typeof(pokemonRepository.getAll()[property]));
 });
 
 pokemonRepository.search("Pikachu");
